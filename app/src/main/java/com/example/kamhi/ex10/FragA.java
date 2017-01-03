73b8cdc079ae9bc350172b45fddc10374887e0c0
amitkamhi <amit.kamhi@gmail.com>
2017-01-03 16:59:43 +0200

    3.1 start

---
+++ app/src/main/java/com/example/kamhi/ex10/FragA.java
@@ -0,0 +1,31 @@
+package com.example.kamhi.ex10;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kamhi on 3/1/2017.
+ */
+
+public class FragA extends Fragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/kamhi/ex10/FragB.java
@@ -0,0 +1,12 @@
+package com.example.kamhi.ex10;
+
+import android.app.Fragment;
+
+/**
+ * Created by Kamhi on 3/1/2017.
+ */
+
+public class FragB extends Fragment {
+
+
+}
